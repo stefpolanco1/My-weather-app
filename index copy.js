ff32e18530b46ee6cd4ebac3d9dd4067f0ae64a4
stefpolanco1 <78278343+stefpolanco1@users.noreply.github.com>
2021-03-21 16:58:13 -0400

    Transferred over html, css and js files into github

---
+++ index copy.js
@@ -0,0 +1,54 @@
+function editDay(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+
+function displayWeatherCondition(response) {
+  console.log(response.data);
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector(".temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].main;
+}
+
+function search(event) {
+  event.preventDefault();
+  let city = document.querySelector("#city-input").value;
+  let units = "imperial";
+  let apiKey = "bc7335652f55946eb62094f23f654f47";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let dateElement = document.querySelector("#date");
+let currentTime = new Date();
+let formElement = document.querySelector("#search-form");
+
+formElement.addEventListener("submit", search);
+
+dateElement.innerHTML = editDay(currentTime);
